93a26af596973605577863a26185b2846b38f943
LukeKimm <rlaxorud0331@gmail.com>
2022-11-07 10:01:46 +0000

    add V2X congestion project python code for graph

---
+++ V2X_python_graph/(ref)V2X_vriables_CBR.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+data_f = open("V2X_variables.csv",'r', encoding='utf-8-sig')
+
+TIMEs = []
+CBRs = []
+ITTs = []
+
+for line in data_f:
+    (TIME, CBR, ITT) = line.split(',')
+    TIMEs.append(TIME)
+    CBRs.append(CBR)
+    ITTs.append(ITT)
+    
+data_f.close()
+
+# 첫번째 열에 항목이 있을 경우 사용
+# TIMEs = TIMEs[1:]
+# CBRs = CBRs[1:]
+# ITTs = ITTs[1:]
+
+TIMEs = [float (i) for i in TIMEs]
+CBRs = [float (i) for i in CBRs]
+ITTs = [float (i) for i in ITTs]
+
+ITTs.insert(0, 0)
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+plt.plot(TIMEs, CBRs, color='magenta', marker='o', linestyle='dashed', label='CBR')
+plt.title("CBR stabilizing")
+plt.xlabel("TIME")
+plt.ylabel("CBR")
+plt.grid(True)
+plt.legend(loc='upper left')
+plt.savefig('V2X_variables_10s_CBR.png')
